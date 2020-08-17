1d7d49df6c7e6ebf935735fceeb0d3ccb344ed2e
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-17 03:04:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,11 +29,11 @@ def can_be_created_in_a_block(args = {title: "Movie", release_date:"1", director
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = :title
-    m.release_date = :release_date
-    m.director = :director
-    m.lead = :lead
-    m.in_theaters = :in_theaters
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.director = args[:director]
+    m.lead = args[:lead]
+    m.in_theaters = args[:in_theaters]
     m.save
   end
 end
