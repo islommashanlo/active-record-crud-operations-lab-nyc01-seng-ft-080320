418c0306af50175377c8e12d347e38356570855e
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-17 03:12:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -95,7 +95,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   mov = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  mov.delete
+  mov.destroy
 end
 
 def can_destroy_all_items_at_once
